2aafdbaa836085335cbbbda4c6e68a168dc4cc20
surik316 <60616688+surik316@users.noreply.github.com>
2021-10-23 22:43:45 +0300

    Initial Commit

---
+++ CovidTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidTracker
+//
+//  Created by maksim.surkov on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
