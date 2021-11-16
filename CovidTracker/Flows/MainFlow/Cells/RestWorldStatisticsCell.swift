b35d2b71e9c8604f29b76c8354ef475d338be9cd
surik316 <60616688+surik316@users.noreply.github.com>
2021-11-16 22:37:08 +0300

    main: added table view rxswift

--- CovidTracker/Flows/MainFlow/Cells/RestWorldStatisticsCell.swift
+++ CovidTracker/Flows/MainFlow/Cells/RestWorldStatisticsCell.swift
@@ -9,8 +9,9 @@ import UIKit
 import RxSwift
 import SnapKit
 import Charts
+import RxCocoa
+
 struct CellModel {
-    var cellNumber: Int?
     let countryName: String
     let totalCases: Int
     let todayCases: Int
@@ -34,21 +35,42 @@ class RestWorldStatisticsCell: UITableViewCell {
     private var activeCovidCases = [CellModel]()
     private var deathCovidCases = [CellModel]()
     private let identifierCell = "cellResetWorld"
-    lazy var rowsToDisplay = [CellModel]()
+    lazy var rowsToDisplay = PublishSubject<[CellModel]>()
     private let disposeBag = DisposeBag()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
+        setUpBindings()
         configureUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupUI()
+        setUpBindings()
         configureUI()
+        
     }
     override func layoutSubviews() {
         super.layoutSubviews()
+    }
+    private func setUpBindings() {
+        segmentedControl.rx.controlEvent(.valueChanged).withLatestFrom(segmentedControl.rx.value).subscribe(onNext : { value in
+            switch value {
+            case 0:
+                self.rowsToDisplay.onNext(self.totalCovidCases)
+            case 1:
+                self.rowsToDisplay.onNext(self.activeCovidCases)
+            default:
+                self.rowsToDisplay.onNext(self.deathCovidCases)
+            }
+        })
+        .disposed(by: disposeBag)
+        rowsToDisplay.asObservable().bind(to: tableView.rx.items(cellIdentifier: identifierCell)) { row, model, cell in
+            if let cell = cell as? CellTableView {
+                cell.configure(from: model, row: row)
+            }
+        }.disposed(by: disposeBag)
     }
     private func setupUI() {
         addSubview(segmentedControl)
@@ -58,18 +80,6 @@ class RestWorldStatisticsCell: UITableViewCell {
             make.trailing.equalToSuperview().offset(-10)
             make.height.equalTo(50)
         }
-        segmentedControl.rx.controlEvent(.valueChanged).withLatestFrom(segmentedControl.rx.value).subscribe(onNext : { value in
-            switch value {
-            case 0:
-                self.rowsToDisplay = self.totalCovidCases
-            case 1:
-                self.rowsToDisplay = self.activeCovidCases
-            default:
-                self.rowsToDisplay = self.deathCovidCases
-            }
-            self.tableView.reloadData()
-        })
-        .disposed(by: disposeBag)
         addSubview(tableView)
         tableView.snp.makeConstraints { make in
             make.top.equalTo(segmentedControl.snp.bottom).offset(5)
@@ -77,7 +87,6 @@ class RestWorldStatisticsCell: UITableViewCell {
             make.trailing.equalToSuperview().offset(-10)
             make.bottom.equalToSuperview().offset(-15)
         }
-        tableView.dataSource = self
         tableView.delegate = self
         tableView.register(CellTableView.self, forCellReuseIdentifier: identifierCell)
         
@@ -93,10 +102,10 @@ class RestWorldStatisticsCell: UITableViewCell {
         self.totalCovidCases = model.totalCovidCases
         self.activeCovidCases = model.activeCovidCases
         self.deathCovidCases = model.deathCovidCases
-        rowsToDisplay = totalCovidCases
+        self.rowsToDisplay.onNext(self.totalCovidCases)
     }
 }
-extension RestWorldStatisticsCell: UITableViewDataSource {
+extension RestWorldStatisticsCell {
     class CellTableView: UITableViewCell {
         private let cellNumberLabel = UILabel()
         private let countryLabel = UILabel()
@@ -152,9 +161,8 @@ extension RestWorldStatisticsCell: UITableViewDataSource {
             totalCasesLabel.font = .systemFont(ofSize: 15, weight: .bold)
             todayCasesLabel.font = .systemFont(ofSize: 10, weight: .bold)
         }
-        func configure(from model: CellModel) {
-            guard let number = model.cellNumber else { return }
-            cellNumberLabel.text = String(number)
+        func configure(from model: CellModel, row: Int) {
+            cellNumberLabel.text = String(row)
             countryLabel.text = model.countryName
             totalCasesLabel.text = model.totalCases.formattedWithSeparator
             todayCasesLabel.text = "+" + model.todayCases.formattedWithSeparator
@@ -162,19 +170,6 @@ extension RestWorldStatisticsCell: UITableViewDataSource {
         }
         
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rowsToDisplay.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifierCell, for: indexPath) as! CellTableView
-        var model = rowsToDisplay[indexPath.row]
-        model.cellNumber = indexPath.row
-        cell.configure(from: model)
-        return cell
-    }
-    
-    
 }
 extension RestWorldStatisticsCell: UITableViewDelegate {
     
